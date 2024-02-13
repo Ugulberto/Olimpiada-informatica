029b8b03a8afcdf3fccfcfdfae14b8c1b23ebf75
Ugulberto <david.blaigl@gmail.com>
2024-02-13 18:28:29 +0100

    Actualizacion 4

--- main.cpp
+++ main.cpp
@@ -1,18 +1,64 @@
 #include <bits/stdc++.h>
 using namespace std;
+struct pedido
+{
+    int prioridad;
+    string id;
+    int dia;
+    char premium;
+};
 
-bool solve() {
-    vector<int> nums; 
-    for (int i = 0; i < 10; i++) {
-        nums.push_back(i);
+bool operator>(pedido a, pedido b)
+{
+    if (a.dia != b.dia)
+    {
+        return b.dia < a.dia;
     }
-    for (auto i : nums) {
-        cout << i << "\n";
+    else
+    {
+        return b.prioridad < a.prioridad;
+    }
+}
+
+bool solve()
+{
+    int cosas;
+    cin >> cosas;
+    int dia = 1, entregados = 0;
+    priority_queue<pedido, vector<pedido>, greater<pedido>> pq;
+    while (cosas--)
+    {
+        int a, c;
+        string b;
+        char d;
+        cin >> a >> b >> c >> d;
+        if (d == 'P')
+            a++;
+        pedido p = {a, b, c, d};
+        pq.push(p);
+    }
+    while (dia <= 30)
+    {
+        int paq = 0;
+        if (pq.top().dia == dia) {
+            cout << "#" << dia << "\n";
+        }
+        while (!pq.empty() && paq < 10 && pq.top().dia == dia)
+        {
+            pedido a = pq.top(); pq.pop();
+            cout << a.id << "\n";
+            paq++;
+        }
+        dia++;
+        if (pq.empty()) {
+            break;
+        }
     }
     return false;
 }
 
-int main() {
+int main()
+{
     while (solve());
     return 0;
 }
