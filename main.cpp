cd024c34870335f558c8541e0899b7136358acac
Ugulberto <david.blaigl@gmail.com>
2024-02-13 17:10:34 +0100

    Actualizacion 3

--- main.cpp
+++ main.cpp
@@ -1,9 +1,15 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
 bool solve() {
-    
-    return true;
+    vector<int> nums; 
+    for (int i = 0; i < 10; i++) {
+        nums.push_back(i);
+    }
+    for (auto i : nums) {
+        cout << i << "\n";
+    }
+    return false;
 }
 
 int main() {
